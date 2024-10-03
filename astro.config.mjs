19639a0e5e136aa9d7f97737adac7d7e5319a9ab
APRENDER.digital <info@aprender.digital>
2024-10-03 17:05:28 -0300

    teste com blog atualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'http://criatividade.digital',
+	base: '/super-apague',
 	integrations: [mdx(), sitemap(), react()],
 });
---
+++ comandos.md
@@ -0,0 +1,6 @@
+## Geração da versão estática
+* npm run build
+## Teste local
+* npm run dev --server
+## Verifica package.json e estala o que não está disponível
+* npm install
