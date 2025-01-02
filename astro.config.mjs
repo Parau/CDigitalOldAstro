be008be1820ee046b52f1cd5626320fcdd6aa0f9
APRENDER.digital <info@aprender.digital>
2025-01-02 18:46:50 -0300

    coloquei para aumentar a compatibilidade do markdown
    Mas em alguns poucos testes não vi diferença.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,10 @@ export default defineConfig({
 	site: 'http://criatividade.digital',
 	//base: '/super-apague',
 	integrations: [mdx(), sitemap(), react()],
+	markdown: {
+    // Opções para o processador de Markdown (ex: remark)
+    remarkPlugins: [],
+    rehypePlugins: [],
+    smartypants: true, // Habilita aspas tipográficas e outros ajustes
+  },
 });
